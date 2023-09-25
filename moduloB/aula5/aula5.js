894361c1d68ed05f2c8d1816a6ec90526ed7a7bf
FelipeVieira4 <felipevieira58238@gmail.com>
2023-09-25 20:17:17 -0300

    adicionado ; no final da linha kkk

--- moduloB/aula5/aula5.js
+++ moduloB/aula5/aula5.js
@@ -4,6 +4,6 @@ console.log("Tipo da 'num1':"+typeof num1);
 
 console.log(Math.cos(num1+num2)); //Utilizando a função "cos" da biblioteca Math
 
-num1 = "Google"
+num1 = "Google";
 
 console.log("Agora o tipo é:"+typeof num1);
